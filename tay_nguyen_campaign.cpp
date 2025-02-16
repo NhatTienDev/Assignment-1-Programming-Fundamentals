3e6eae810294bc6c2c12c88222b034c1fea8be3a
ADMIN\Administrator <tien.nguyen20020713@gmail.com>
2025-02-16 16:18:47 +0700

    Done task 0 Complete task 2.2 and checked some basic testcase for task 1, 2.1 and 2.2

--- tay_nguyen_campaign.cpp
+++ tay_nguyen_campaign.cpp
@@ -87,7 +87,7 @@ bool readFile(const string &filename, int LF1[], int LF2[], int &EXP1, int &EXP2
 
     if(E > 99) E = 99;
     if(E < 0) E = 0;
-    
+
     return true;
 }
 
@@ -176,9 +176,102 @@ string determineRightTarget(const string &target)
     return "INVALID"; // if has more than 3 integers found
 }
 
+const string ACTUAL_TARGET[5] = {"Buon Ma Thuot", "Duc Lap", "Dak Lak", "National Route 21", "National Route 14"};
+
+bool isMatchedTarget(const string &decodedMessage)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        if(decodedMessage == ACTUAL_TARGET[i]) return true;
+    }
+
+    return false;
+}
+
+string toLowerCase(const string &s)
+{
+    string lowerCaseString = s;
+
+    for(int i = 0; i < lowerCaseString.length(); i++)
+    {
+        if(lowerCaseString[i] >= 'A' && lowerCaseString[i] <= 'Z')
+        {
+            lowerCaseString[i] = lowerCaseString[i] + 32;
+        }
+    }
+
+    return lowerCaseString;
+}
+
+string CaesarCipherMethod(const string &message, int shift)
+{
+    string decodedMessage = "";
+
+    for(int i = 0; i < message.length(); i++)
+    {
+        char ch = message[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+        {
+            decodedMessage += char(((ch - 'A' - shift + 26) % 26) + 'A');
+        }
+        else if(ch >= 'a' && ch <= 'z')
+        {
+            decodedMessage += char(((ch - 'a' - shift + 26) % 26) + 'a');
+        }
+        else if(ch == ' ')
+        {
+            decodedMessage += ch;
+        }
+        else
+        {
+            return "INVALID"; // if character is not A-Z, a-z, ' '
+        }
+    }
+
+    return decodedMessage;
+}
+
+string reversedStringMethod(const string &message)
+{
+    string reversed = message;
+    int len = reversed.length();
+
+    for(int i = 0; i < len/2; i++)
+    {
+        char temp = reversed[i];
+        reversed[i] = reversed[len - i - 1];
+        reversed[len - i - 1] = temp;
+    }
+
+    return reversed;
+}
+
 string decodeTarget(const string &message, int EXP1, int EXP2)
 {
     // TODO: Implement this function
+    string decodedMessage;
+
+    if(EXP1 >= 300 && EXP2 >= 300)
+    {
+        int shift = (EXP1 + EXP2) % 26;
+        decodedMessage = CaesarCipherMethod(message, shift);
+        
+        if(isMatchedTarget(decodedMessage) == true) return decodedMessage;
+    }
+    if(EXP1 < 300 || EXP2 < 300)
+    {
+        decodedMessage = reversedStringMethod(message);
+
+        for(int i = 0; i < 5; i++)
+        {
+            if(toLowerCase(decodedMessage) == toLowerCase(ACTUAL_TARGET[i]))
+            {
+                return ACTUAL_TARGET[i];
+            }
+        }
+    }
+
     return "INVALID";
 }
 
